9bd695f4b9cfc91af0448be0bc53757424def0ce
afsaanchez04 <afsanchez04@gmail.com>
2024-10-21 07:32:51 -0500

    tienda-naranjas

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <nav>Menú de navegación</nav>
+      </header>
+      <section>Sección promos 🍊</section>
+      <section>Sección tienda 🏪</section>
+      <footer>Pie de página</footer>
+    </div>
+  )
+
+}
+
+export default App
